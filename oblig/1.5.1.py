47f92eabc00ee1e6adfd4659caf892736561ef9d
David Pearson <david.d.pearson@outlook.com>
2025-09-16 16:28:05 +0200

    Completed task 1.4 and began on task 1.5

---
+++ oblig/1.5.1.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+import numpy as np
+
+file_path = 'oblig\WineQT.csv' # you might have to change this to run locally
+wine = pd.read_csv(file_path) 
+wine = wine.drop(columns=['Id'])
+
+X = wine.drop(columns=['quality'])
+y = wine['quality']
+
+skfolds = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+pipe = Pipeline([
+    ("scale", StandardScaler()),
+    ("model", LinearRegression())
+])
+
+r2 = cross_val_score(pipe, X, y, cv=skfolds, scoring='r2')
+mse = cross_val_score(pipe, X, y, cv=skfolds, scoring='neg_mean_squared_error')
+rmse = cross_val_score(pipe, X, y, cv=skfolds, scoring='neg_root_mean_squared_error')
+
+print("Cross Validation Scores\n R2:", r2, "\n", "MSE:", mse, "\n", "RMSE:", rmse)
+
+meanR = np.mean(r2) 
+meanM = np.mean(mse) 
+meanRM = np.mean(rmse)
+variance = np.std(r2, ddof=1) 
+varM = np.std(mse, ddof=1) 
+varRM = np.std(rmse, ddof=1)
+print("Mean result:" , "R^2", meanR, " MSE: ", meanM, " RMSE: ", meanRM )
+print("Variance:", "R^2", variance, " MSE: ", varM, " RMSE: ", varRM )
---
+++ oblig/task1.4.1.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import StratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import SGDRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+import numpy as np
+
+
+file_path = 'oblig\WineQT.csv' # you might have to change this to run locally
+wine = pd.read_csv(file_path)
+X = wine[['alcohol']]
+y = wine['quality']
+
+pipe = Pipeline([
+    ("scale", StandardScaler()),
+    ("model", SGDRegressor())
+])
+
+
+
+skfolds = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+r2 = cross_val_score(pipe, X, y, cv=skfolds, scoring='r2')
+mse = cross_val_score(pipe, X, y, cv=skfolds, scoring='neg_mean_squared_error')
+rmse = cross_val_score(pipe, X, y, cv=skfolds, scoring='neg_root_mean_squared_error')
+
+print("Cross Validation Scores\n R2:", r2, "\n", "MSE:", mse, "\n", "RMSE:", rmse)
+
+meanR = np.mean(r2) 
+meanM = np.mean(mse) 
+meanRM = np.mean(rmse)
+variance = np.std(r2, ddof=1) 
+varM = np.std(mse, ddof=1) 
+varRM = np.std(rmse, ddof=1)
+print("Mean result:" , "R^2", meanR, " MSE: ", meanM, " RMSE: ", meanRM )
+print("Variance:", "R^2", variance, " MSE: ", varM, " RMSE: ", varRM )
+
+
+
+
+
