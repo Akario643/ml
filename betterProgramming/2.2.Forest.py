961b57fd416ad04a83bf232cab39f5225f5dfa4e
Akario643 <daviderbest@outlook.com>
2025-09-24 16:25:14 +0200

    added parameters to forest

--- betterProgramming/2.2.Forest.py
+++ betterProgramming/2.2.Forest.py
@@ -1,10 +1,16 @@
-from helperFunctions import wine, pipeline, scoring
+from helperFunctions import wine, pipeline, scoring, skfolds
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
 ## Select Attributes
 X = wine.drop(columns=['quality'])
 y = wine['quality']
 
 ## Create model and show score
-model = pipeline(2, True, RandomForestRegressor())
-score = scoring(model, X, y, False)
+pipe = pipeline(1, False, RandomForestRegressor())
+parameters = {'model__n_estimators':[100,200,400,500], 'model__max_depth':[2,5,8,10,20], "model__min_samples_split":[2,5,10,15], "model__min_samples_leaf":[1,3,4,7]}
+grid = GridSearchCV(pipe, parameters, cv=skfolds)
+grid.fit(X,y)
+model_grid = grid.best_estimator_
+
+score = scoring(model_grid, X,y,False)
