a5de57fbf6385e3babb9e86dbcbcaebb97a91472
Akario643 <daviderbest@outlook.com>
2025-09-24 16:24:47 +0200

    added parameters to decisiontree

--- betterProgramming/2.2.decision.py
+++ betterProgramming/2.2.decision.py
@@ -1,10 +1,16 @@
-from helperFunctions import wine, pipeline, scoring
+from helperFunctions import wine, pipeline, scoring, skfolds
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import GridSearchCV
 
 ## Select Attributes
 X = wine.drop(columns=['quality'])
 y = wine['quality']
 
 ## Create model and show score
-model = pipeline(2, True, DecisionTreeRegressor())
-score = scoring(model, X, y, False)
+pipe = pipeline(1, False, DecisionTreeRegressor())
+parameters = {'model__max_depth':[2,5,8,10,None], "model__min_samples_leaf":[1,2,4,6], "model__min_samples_split":[2,5,10,15]}
+grid = GridSearchCV(pipe, parameters, cv=skfolds)
+grid.fit(X,y)
+model_grid = grid.best_estimator_
+
+score = scoring(model_grid, X,y,False)
